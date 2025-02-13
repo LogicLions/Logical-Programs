ab134eef596d66278e29e3db0d7636fbaa858044
LogicLions <sachinyt2024@gmail.com>
2025-02-13 20:18:50 +0530

    Adding double pointer in pointers program

--- Pointers-Simple.cpp
+++ Pointers-Simple.cpp
@@ -4,13 +4,21 @@ using namespace std;
 int main()
 {
     int a = 10;
-    int* aPtr = &a; // storing address of memory location of a. int* to store address of int datatype.
+    int* p = &a; // storing address of memory location of a. int* to store address of int datatype.
 
-    cout<<"Memory address - aPtr: "<<aPtr;
-    cout<<"\nValue stored at aPtr: "<<*aPtr;
+    cout<<"Value of a: "<<a;
+    cout<<"\nMemory address - p: "<<p;
+    cout<<"\nValue stored at p: "<<*p;
 
-    *aPtr = 20; // assigning new value in a using its pointer means using memory address.
-    cout<<"\nNew Value stored at aPtr: "<<*aPtr;
+    *p = 20; // assigning new value in a using its pointer means using memory address.
+    cout<<"\nNew Value stored at p: "<<*p;
+
+    // Double pointer (pointer to pointer)
+    int **q=&p; // storing address of pointer p inside pointer q;
+
+    cout<<"\nMemory adress - q: "<<q; // it will print address stored at q pointer
+    cout<<"\nDereferncing q once: "<<*q; // it will print address stored at p pointer
+    cout<<"\nDereferencing q twice: "<<**q; // it will print value of a;
 
     return 0;
 }
