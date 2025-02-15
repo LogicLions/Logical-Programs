3b6a693057e978652d1e746d04b1256e1cd76eb3
LogicLions <sachinyt2024@gmail.com>
2025-02-15 19:11:02 +0530

    Correcting Stack Operations program

--- Stack-Operations.cpp
+++ Stack-Operations.cpp
@@ -4,50 +4,106 @@ using namespace std;
 struct node
 {
     int data;
-    struct node *next;
+    struct node* next;
 };
 
 typedef struct node NODE;
-typedef struct node *PNODE;
+typedef struct node* PNODE;
 
 class Stack
 {
     private:
         PNODE TOP;
-        int iCount;
-
+        int count;
     public:
-        Stack();
-        void display();
-        int GetCount();
-        void Push(int no);
-        void Pop();
-        int GeTop();
-        bool isEmpty();
+        Stack()
+        {
+            TOP = NULL;
+            count = 0;
+        }
+
+        void Push(int data)
+        {
+            PNODE temp=new NODE;
+            temp->data = data;
+            temp->next = NULL;
+
+            if(TOP == NULL)
+            {
+                TOP = temp;
+            }
+            else
+            {
+                temp->next = TOP;
+                TOP = temp;
+            }
+
+            count++;
+        }
+
+        void Pop()
+        {
+            if(TOP == NULL)
+            {
+                cout<<"Stack is already empty."<<endl;
+            }
+            else
+            {
+                PNODE temp = TOP;
+                TOP = TOP->next;
+                cout<<"Popped element is :"<<temp->data<<endl;
+                delete temp;
+            }
+            
+            count--;
+        }
+
+        void Display()
+        {
+            PNODE temp;
+            temp = TOP;
+            cout<<"Stack :"<<endl;
+            while(temp != NULL)
+            {                
+                cout<<temp->data<<endl;
+                temp=temp->next;
+            }
+        }
+
+        int GetTop()
+        {
+            if(TOP == NULL)
+            {
+                cout<<"Stack is empty."<<endl;
+            }
+            else
+            {
+                cout<<"TOP Element is: "<<TOP->data<<endl;
+            }
+        }
+
+        int GetCount()
+        {
+            cout<<"Count :"<<count<<endl;
+        }
 };
-
-Stack :: Stack()
-{
-    TOP=NULL;
-    iCount = 0;
-}
-
-void Stack :: Push(int no)
-{
-    PNODE temp = NULL;
-    temp = new NODE;
-
-    
-
-}
-
-int Stack :: GetCount()
-{
-    return iCount;
-}
 
 int main()
 {
+    Stack stack;
+    stack.Push(10);
+    stack.Push(20);
+    stack.Push(30);
+    stack.Push(40);
+
+    stack.GetCount();
+    stack.Display();
+
+    stack.Pop();
+    stack.Display();
+
+    stack.Pop();
+    stack.Display();
+
     return 0;
 }
-
