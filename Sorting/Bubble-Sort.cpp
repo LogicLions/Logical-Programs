cb0cee2ff459b7cd4404621026346776b22c4c66
LogicLions <sachinyt2024@gmail.com>
2025-02-21 11:19:51 +0530

    Modifying Bubble Sort program

--- Sorting/Bubble-Sort.cpp
+++ Sorting/Bubble-Sort.cpp
@@ -1,23 +1,33 @@
 #include<iostream>
 using namespace std;
 
+void PrintArray(int n, int arr[])
+{
+    for(int k=0;k<n;k++)
+    {
+        cout<<arr[k]<<"\t";
+    }
+}
+
 void BubbleSort(int n, int arr[])
 {
     for(int i=0;i<n-1;i++)
     {
-        for(int j=0;j<=n-i-1;j++)
+        bool isSwap =false;
+        cout<<"Iteration: "<<i+1<<endl;
+        for(int j=0;j<n-i-1;j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j],arr[j+1]);
+                isSwap = true;
             }
         }
-    }
 
-    cout<<"Sorted array in ascending order is:"<<endl;
-    for(int k=0;k<n;k++)
-    {
-        cout<<arr[k]<<endl;
+        if(!isSwap) // Optimization if array gets sorted within first iteration or if its already sorted then it will return after first iteration. 
+        {
+            return;
+        }
     }
 }
 
@@ -36,5 +46,8 @@ int main()
     }
 
     BubbleSort(n, arr);
+    cout<<"Sorted array is:"<<endl;
+    PrintArray(n, arr);
+
     return 0;
 }
